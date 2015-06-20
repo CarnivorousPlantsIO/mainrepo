653b42cda1e9dc1a8c79e28abf7d84d5d9a2cc36
Emmanuel Gomes Brandao <emmanuel.brandao@lambda3.com.br>
2015-06-20 13:01:58 -0300

    Nodejs code

---
+++ nodejs/app.js
@@ -0,0 +1,56 @@
+﻿console.log('hi');
+
+var azureQueue = require('azure-queue-node');
+
+azureQueue.setDefaultClient({
+    accountUrl: 'http://vitormeriat.queue.core.windows.net/',
+    accountName: 'carnivorousplants',
+    accountKey: 'sKbl4rdIxzFYk2I3Y30vNZLME0KcT2EtCGZ/2pw9dCuY+eZruEnpDKMSYheFGCl0YsfUfS1bmPZDwiuhVSW3Ng=='
+});
+
+var queueName = 'telimetria';
+
+require('cylon').robot({
+    
+    connections: { edison: { adaptor: 'intel-iot' } },
+    devices: {
+        //list all of the sensors, lights, etc. that you will use
+        led: { driver: 'led', pin: A0 },
+        led: { driver: 'ledGreen', pin: 6 },
+        led: { driver: 'ledRed', pin: 5 },
+        led: { driver: 'ledBlue', pin: 7 },
+        led: { driver: 'releligado', pin: 8 }
+    },
+    
+    // this is where you write the main part of your app
+    work: function (my) {
+        every((0.10).second(), function () {
+            //my.led.toggle();
+            azureQueue.createQueue(queueName, true, cb);
+            
+            azureQueue.createMessage(queueName, "Hello world Carnivorous Plants!", function (error, result, response) {
+                if (!error) {
+                    // Message inserted
+                }
+            });
+        });
+    }
+
+    
+}).start();
+
+
+
+
+//connections: { edison: { adaptor: 'intel-iot' } },
+//devices: {
+//    //list all of the sensors, lights, etc. that you will use
+//    led: { driver: 'led', pin: 13 }
+//},
+
+//// this is where you write the main part of your app
+//work: function (my) {
+//    every((0.1).second(), function () {
+//        //my.led.toggle();
+//    });
+//}
---
+++ nodejs/config.js
@@ -0,0 +1,11 @@
+﻿//this file holds configuration variables
+//you can add any that you may need for your application
+
+module.exports = {
+    host: 'chica.local',
+    user: 'root',
+    password: '123!@#qweQWE',
+    projectName: 'plants',
+    startFile: 'app.js',
+    sshPort: 22
+}
